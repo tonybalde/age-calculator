203480893725fec2ea40653b87beaa7c18380689
Tony Baldessari <tonybalde@hotmail.com>
2024-06-24 21:07:25 -0300

    initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/App.css"
+import arrow from './assets/images/icon-arrow.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <div className="numbers-container">
+          <div className="day-container">
+            <h1 className="title-input">D A Y</h1>
+            <input className="day-number input-field" min={1} max={31} placeholder="DD"/>
+          </div>
+          <div className="month-container">
+            <h1 className="title-input">M O N T H</h1>
+            <input className="month-number input-field" min={1} max={12} placeholder="MM"/>
+          </div>
+          <div className="year-container">
+            <h1 className="title-input">Y E A R</h1>
+            <input className="year-number input-field" min={1} max={2024} placeholder="YYYY"/>
+          </div>
+        </div>
+        <div className="arrow-container">
+          <img src={arrow} className="arrow"/>
+        </div>
+        <div className="calculations-container">
+          <h1 className="calculations-text"><span className="dashes">--</span> Years</h1>
+          <h1 className="calculations-text"><span className="dashes">--</span> Months</h1>
+          <h1 className="calculations-text"><span className="dashes">--</span> Days</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
